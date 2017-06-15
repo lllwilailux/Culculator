c48b6bf8082450bda96bf002eae26e5f2d5c03aa
lllwilailux <lllwilailux@gmail.com>
2017-06-15 23:35:19 +0700

    add function convet digit,distance,temp

--- app/src/main/java/com/example/augmentis/culculator/MainActivity.java
+++ app/src/main/java/com/example/augmentis/culculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.augmentis.culculator;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
         btnMinus = (Button) findViewById(R.id.Btnminus_id);
         btnMulti = (Button) findViewById(R.id.Btnmulti_id);
         btnDivide = (Button) findViewById(R.id.Btndivide_id);
+
+        Celsius = (RadioButton) findViewById(R.id.RbCelsius_id);
+        Fahrenheit = (RadioButton) findViewById(R.id.RbFahrenheit_id);
+
+        Bin = (RadioButton) findViewById(R.id.RbBin);
+        Dec = (RadioButton) findViewById(R.id.RbDec);
+        Hex = (RadioButton) findViewById(R.id.RbHex);
+        Oct = (RadioButton) findViewById(R.id.RbOct);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -161,6 +172,92 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        temperature = (RadioGroup) findViewById(R.id.temperature);
+        temperature.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (showResult.getText().length() > 0) {
+                    setEnabledconvertDigit(false);
+                    Float inputValue = Float.parseFloat(showResult.getText().toString());
+                    if (checkedId == R.id.RbCelsius_id) {
+                        showResult.setText(String.valueOf(convertToCelsius(inputValue)));
+                    }
+                    if (checkedId == R.id.RbFahrenheit_id) {
+                        showResult.setText(String.valueOf(convertToFahrenhrit(inputValue)));
+                    }
+                }
+            }
+        });
+
+        convertDigit = (RadioGroup) findViewById(R.id.convertDigit);
+        convertDigit.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (showResult.getText().toString().length() > 0) {
+                    String inputValue = showResult.getText().toString();
+                    if (checkedId == R.id.RbHex) {
+                        showResult.setText(convertDigit(inputValue,digiTemp,16));
+                        digiTemp = 16;
+                    }
+                    if (checkedId == R.id.RbBin) {
+                        showResult.setText(convertDigit(inputValue,digiTemp,2));
+                        digiTemp = 2;
+                    }
+                    if (checkedId == R.id.RbOct) {
+                        showResult.setText(convertDigit(inputValue,digiTemp,8));
+                        digiTemp = 8;
+                    }
+                    if (checkedId == R.id.RbDec) {
+                        showResult.setText(convertDigit(inputValue,digiTemp,10));
+                        digiTemp = 10;
+                    }
+                }
+            }
+        });
+
+        distanceBetween = (RadioGroup) findViewById(R.id.diatanceBetween);
+        distanceBetween.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (showResult.getText().toString().length() > 0) {
+                    setEnabledconvertDigit(false);
+                    Float inputValue = Float.parseFloat(showResult.getText().toString());
+                    if (checkedId == R.id.RbMiles){
+                        showResult.setText(String.valueOf(convertToMiles(inputValue)));
+                    }
+                    if (checkedId == R.id.RbKilometer) {
+                        showResult.setText(String.valueOf(convertToKilimeters(inputValue)));
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void setEnabledconvertDigit(boolean enabled) {
+        for (int i = 0; i < convertDigit.getChildCount(); i++) {
+            convertDigit.getChildAt(i).setEnabled(enabled);
+        }
+    }
+
+    private float convertToFahrenhrit(float celsius) {
+        return (float) ((celsius * 1.8000) + 32);
+    }
+
+    private float convertToCelsius(float fahrenheit) {
+        return (float) ((fahrenheit - 32.0) / 1.8000);
+    }
+
+    private String convertDigit(String s, int from, int to) {
+        return new BigInteger(s.trim(), from).toString(to);
+    }
+
+    private float convertToKilimeters(float miles) {
+        return (float) (miles * 1.609344);
+    }
+
+    private float convertToMiles(float kilometers) {
+        return (float) (kilometers * 0.621371192);
     }
 
     public void insert(String s) {
@@ -169,7 +266,7 @@ public class MainActivity extends AppCompatActivity {
         showResult.setText(str);
     }
 
-    public void reset(){
+    public void reset() {
         str = "";
         op = 'q';
         num = 0;
